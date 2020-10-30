560a5e666f68c851957f5aea143be9cad4db7c10
ShenQi1996 <qisam1989@gmail.com>
2020-10-30 12:54:00 -0400

    not done

--- src/components/section2/section2.component.jsx
+++ src/components/section2/section2.component.jsx
@@ -6,9 +6,7 @@ import profile from "./profile.svg";
 
 const Section2Styled = styled.div`
   /** font-family: 'Bebas Neue', cursive; */
-  h1{
-    color: white;
-  }
+  
 `;
 
 export default function Section2() {
@@ -16,17 +14,27 @@ export default function Section2() {
     <Section2Styled id="section2">
       <Wrapper>
         <h1 >About Me</h1>
-        <div>
-          <div>
             <img className="proImage" src={profile}/>
-               <p className="bio">
-              My attached resume outlines all that I could offer your organization. Here's the overview:
-My educational background has prepared me for the role of becoming a member of organization.  In particular, my study of Computer Science has given me a solid background so that I can perform basic programming. I am eager to contribute my enthusiasm and up-to-date skills to your organization.
-I am certain that my resume will give you a greater understanding of my qualifications for this exciting opportunity.
-I would greatly appreciate the opportunity to work with and learn from you. I look forward to speaking with you soon.
-              </p>
-          </div>
-        </div>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <p className="bio">
+                My attached resume outlines all that I could offer your organization. Here's the overview:
+  My educational background has prepared me for the role of becoming a member of organization.  In particular, my study of Computer Science has given me a solid background so that I can perform basic programming. I am eager to contribute my enthusiasm and up-to-date skills to your organization.
+  I am certain that my resume will give you a greater understanding of my qualifications for this exciting opportunity.
+  I would greatly appreciate the opportunity to work with and learn from you. I look forward to speaking with you soon.
+                  </p>
+                </div>
+                <div class="flip-card-back">
+                <p className="bio">
+                My attached resume outlines all that I could offer your organization. Here's the overview:
+  My educational background has prepared me for the role of becoming a member of organization.  In particular, my study of Computer Science has given me a solid background so that I can perform basic programming. I am eager to contribute my enthusiasm and up-to-date skills to your organization.
+  I am certain that my resume will give you a greater understanding of my qualifications for this exciting opportunity.
+  I would greatly appreciate the opportunity to work with and learn from you. I look forward to speaking with you soon.
+                  </p>
+                </div>
+              </div>
+            </div>
       </Wrapper>
     </Section2Styled>
   );
